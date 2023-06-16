321b6c65dd8ac87b53a607a0618d746470372276
Mario M <107927311+19mario19@users.noreply.github.com>
2023-06-16 07:12:53 +0100

    new design

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,23 +2,24 @@ import React, { useEffect, useState } from "react"
 
 function Game() {
   const list = ["rock", "paper", "scissors"]
-  const [selector, setSelector] = useState(list[0])
+  const [selector, setSelector] = useState(undefined)
   const [random, setRandom] = useState("")
   const [count, setCount] = useState({
     triumf: 0,
     equal: 0,
     defeat: 0,
   })
-  const total = count.defeat + count.equal + count.triumf 
+  const total = count.defeat + count.equal + count.triumf
 
   function percent(percent, total) {
-    return percent? (percent * 100) / total : 0
+    return percent ? (percent * 100) / total : 0
   }
 
   function createRandom() {
     const rand = Math.floor(Math.random() * list.length)
     const out = list[rand]
     setRandom(out)
+
     // Adding count
 
     let result = rules(selector, out)
@@ -84,52 +85,75 @@ function Game() {
 
   return (
     <div className="game">
-      <h5>Your choise:</h5>
-      {list &&
-        list.map((el, index) => {
-          return (
-            <li
-              name={el}
-              key={index}
-              className={selector === el ? "select" : ""}
-              onClick={() => selectOption(el)}
-            >
-              {el}
-            </li>
-          )
-        })}
-      <p>
-        Your selection currently is: <span>{selector.toUpperCase()}</span>
-      </p>
+      <div className="top">
+        <div className="count">
+          <p className="triumf">
+            <span>{count.triumf}</span>
+          </p>
+          <p className="equal">
+            <span>{count.equal}</span>
+          </p>
+          <p className="defeat">
+            <span>{count.defeat}</span>
+          </p>
+        </div>
+        <p className="percent">
+          Won <span>{percent(count.triumf, total).toFixed(2)}%</span> from{" "}
+          <span>{total}</span>.
+        </p>
+      </div>
+      <div className="main">
+        <div className="left">
+          <h5>You:</h5>
+          <ul>
+            {list &&
+              list.map((el, index) => {
+                return (
+                  <li
+                    name={el}
+                    key={index}
+                    className={selector === el ? "select" : ""}
+                    onClick={() => selectOption(el)}
+                  >
+                    {el}
+                  </li>
+                )
+              })}
+          </ul>
+          {/* <p>
+            Your selection currently is: <span>{selector.toUpperCase()}</span>
+          </p> */}
+        </div>
+        <div className="right">
+          <h5>Computer's choise:</h5>
+          <ul>
+            {list &&
+              list.map((el, index) => {
+                return (
+                  <li
+                    name={el}
+                    key={index}
+                    className={random === el ? "select" : ""}
+                  >
+                    {el}
+                  </li>
+                )
+              })}
+          </ul>
+          {/* <p>
+            After carefully thinking, the choise is:{" "}
+            <span>
+            {random ? random.toUpperCase() : "press play to discover"}
+            </span>
+          </p> */}
+          </div>
+        </div>
+          <div className="outcome">
+            <p>
+              {random ? rules(selector, random).toUpperCase() : "Waiting..."}
+            </p>
+      </div>
       <button onClick={createRandom}>Play</button>
-      <h5>Computer's choise:</h5>
-      <p>
-        After carefully thinking, the choise is:{" "}
-        <span>{random ? random.toUpperCase() : "press play to discover"}</span>
-      </p>
-      <div className="result">
-        <p>
-          The battle outcomes were assessed accurately, result is:{"\n"}
-          <span>
-            {random ? rules(selector, random).toUpperCase() : "Waiting..."}
-          </span>
-        </p>
-      </div>
-      <div className="result">
-        <p>
-          Triumf <span>{count.triumf}</span>
-        </p>
-        <p>
-          Equal <span>{count.equal}</span>
-        </p>
-        <p>
-          Defeat <span>{count.defeat}</span>
-        </p>
-        <p>
-          You won <span>{percent(count.triumf, total).toFixed(2)}%</span> from{" "}
-          <span>{total}</span> tries.
-        </p>
-      </div>
     </div>
   )
 }
